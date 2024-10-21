c65bdb6fd4327cb27a83c43b980f43917f028545
Piquerez09 <joachim.andradeimbru@gmail.com>
2024-10-21 10:10:37 -0300

    joca

--- aplicativo.py
+++ aplicativo.py
@@ -1,45 +1,3 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from werkzeug.utils import secure_filename
-import os
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['UPLOAD_FOLDER'] = 'app/static/images'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), nullable=False, unique=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(500), nullable=False)
-    image_file = db.Column(db.String(150), nullable=False, default='default.jpg')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    content = db.Column(db.String(500), nullable=False)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-@app.route('/')
-def home():
-    posts = Post.query.all()
-    return render_template('index.html', posts=posts)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -64,45 +22,27 @@ def register():
         flash('Your account has been created! You can now log in', 'success')
         return redirect(url_for('login'))
     return render_template('register.html')
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
-@app.route('/post', methods=['POST'])
-@login_required
-def post():
-    content = request.form.get('content')
-    if 'image' in request.files:
-        image = request.files['image']
-        if image:
-            filename = secure_filename(image.filename)
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            new_post = Post(content=content, image_file=filename, user_id=current_user.id)
-            db.session.add(new_post)
-            db.session.commit()
-    return redirect(url_for('home'))
-
-@app.route('/chat', methods=['GET', 'POST'])
-@login_required
-def chat():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        recipient_username = request.form['recipient']
-        content = request.form['content']
-        recipient = User.query.filter_by(username=recipient_username).first()
-        if recipient:
-            new_message = Message(sender_id=current_user.id, recipient_id=recipient.id, content=content)
-            db.session.add(new_message)
-            db.session.commit()
-            flash('Mensagem enviada!', 'success')
-        else:
-            flash('Usuário não encontrado.', 'danger')
-    users = User.query.all()
-    messages = Message.query.filter((Message.sender_id == current_user.id) | (Message.recipient_id == current_user.id)).all()
-    return render_template('chat.html', users=users, messages=messages)
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('home'))
+        flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html')
 
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(email=email, username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
